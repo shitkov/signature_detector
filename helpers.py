21c1b0e073545e7aafa9e6e664647c5ed2fd2b5c
Konstantin Shitkov <52782381+shitkov@users.noreply.github.com>
2021-09-07 12:48:00 +0300

    Add files via upload

--- helpers.py
+++ helpers.py
@@ -1,8 +1,7 @@
 import PIL
 from PIL import Image, ImageDraw
 
-def draw_bboxs(img_path, preds_list):
-    image = Image.open(img_path)
+def draw_bboxs(image, preds_list):
     image = image.convert('RGB')
     draw = ImageDraw.Draw(image)
     for box in preds_list:
@@ -12,3 +11,14 @@ def draw_bboxs(img_path, preds_list):
         draw.line((coord[2], coord[3], coord[2], coord[1]), fill=(255, 0, 0), width=5)
         draw.line((coord[2], coord[1], coord[0], coord[1]), fill=(255, 0, 0), width=5)
     return image
+
+def resizer(image, fixed_size=1280, grayscale=True):
+    w, h = image.size
+    percent = float(fixed_size / float(max(w, h)))
+    resized_image = image.resize(
+            (int(w * percent), int(h * percent)),
+            PIL.Image.NEAREST
+        )
+    if grayscale:
+        resized_image = resized_image.convert('L') 
+    return resized_image_gray
